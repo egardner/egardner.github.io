0f68ace7eb521b9eefe6ee8fe9dcc0da121ffcf0
Eric Gardner <gardner.ec@gmail.com>
2015-01-06 13:00:41 -0800

    Enable jQuery and set layout to null
    layout: null is required to keep Jekyll from adding the default HTML
    boilerplate to the main.js file once it’s been processed.
    
    jQuery has been enabled for responsive menu feature – this may be
    removed if functionality can be duplicated in Vanilla JS.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,27 @@
 ---
+layout: null
 library: jquery-2.1.3.min.js
 ---
 // If using a JS framework, include it here
-// {% include_relative _lib/{{page.library}} %}
+{% include_relative _lib/{{page.library}} %}
 
 // Application code goes here
+$(document).ready(function(){
+  $('.js-menu-trigger').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+  $('.js-menu-screen').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
+
 
 // Vanilla JS equivalent of $(document).ready
-document.addEventListener('DOMContentLoaded', function(){
-
-});
+//document.addEventListener('DOMContentLoaded', function(){
+//
+//});
