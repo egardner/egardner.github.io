cfbdfcb576beafd056e8c1920cb8b42a1f329ca2
Eric Gardner <gardner.ec@gmail.com>
2016-05-21 17:05:09 -0700

    Add Photoswipe functionality
    Featherlight has been removed in favor of Photoswipe. Implementation is
    the same as in the Getty Roman Mosaics project.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -4,7 +4,9 @@ layout: null
 {% include_relative vendor/jquery.min.js %}
 {% include_relative vendor/jquery.smoothState.js %}
 {% include_relative vendor/slick.min.js %}
-{% include_relative vendor/featherlight.min.js %}
+{% include_relative vendor/underscore-min.js %}
+{% include_relative vendor/photoswipe.min.js %}
+{% include_relative vendor/photoswipe-ui-default.min.js %}
 {% include_relative vendor/jquery.vide.min.js %}
 
 // Helper Functions
@@ -41,6 +43,49 @@ slickSetup = function() {
   }
 };
 
+function lightBox(index) {
+  var pswpElement = document.querySelectorAll(".pswp")[0];
+
+  // build items array
+  var slides  = [];
+  var figures = document.querySelectorAll(".masonry-item");
+  var options = {
+    index: index,
+    clickToCloseNonZoomable: false
+  };
+
+  // document query selector returns an HTMLCollection, not a true array
+  // So we need to proxy a true Array object to get forEach
+  [].forEach.call(figures, function(figure) {
+    var slide   = {};
+    slide.src   = figure.querySelector("img").src;
+    slide.w     = figure.querySelector("img").naturalWidth;
+    slide.h     = figure.querySelector("img").naturalHeight;
+    // slide.title = figure.querySelector("figCaption").textContent;
+    slides.push(slide);
+  });
+
+  // init gallery
+  var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, slides, options);
+  gallery.init();
+}
+
+function lightBoxSetup() {
+  if ($(".masonry-item")) {
+    $figures = $(".masonry-item img");
+    $figures.on("click", function(e) {
+      var figs = document.querySelectorAll(".masonry-item");
+      var target = _.findIndex(figs, function(figure) {
+        return figure.id == e.target.parentNode.id;
+      });
+
+      console.log(target);
+      lightBox(target);
+    });
+  }
+}
+
+
 // Initialization
 
 $(document).ready(function() {
@@ -52,6 +97,7 @@ $(document).ready(function() {
     changeBackground($cover, randomImage);
   }
   slickSetup();
+  lightBoxSetup();
   $('#video').vide({
     mp4: "/assets/video/redsky.mp4",
     webm: "/assets/video/redsky.webm",
